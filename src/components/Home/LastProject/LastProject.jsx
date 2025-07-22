301212c5d752c628438f5040f9f07bc5ff89c43f
CyberBeeDesign <cyberbeedesign@gmail.com>
2025-07-22 07:32:48 +0200

    Update links

--- src/components/Home/LastProject/LastProject.jsx
+++ src/components/Home/LastProject/LastProject.jsx
@@ -12,44 +12,62 @@ const LastProject = () => {
         <h2>My last projects</h2>
         <div className="lastprojectcard-box">
           <div className="lastprojectcard">
-            <img src={cinefyMockup} alt="cinefy-mockup" className="Mockup" />
-            <div className="lastproject-text-container">
-              <div className="lastproject-text">
-                <h3>Cinefy</h3>
-                <p>Website design</p>
+            <a
+              href="https://cinefy-cyberbee.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={cinefyMockup} alt="cinefy-mockup" className="Mockup" />
+              <div className="lastproject-text-container">
+                <div className="lastproject-text">
+                  <h3>Cinefy</h3>
+                  <p>Website design</p>
+                </div>
+                <img src={arrowmore} alt="" className="arrow-more" />
               </div>
-              <img src={arrowmore} alt="" className="arrow-more" />
-            </div>
+            </a>
           </div>
 
           <div className="lastprojectcard">
-            <img
-              src={jammmingMockup}
-              alt="jammming-mockup"
-              className="Mockup"
-            />
-            <div className="lastproject-text-container">
-              <div className="lastproject-text">
-                <h3>Jammming</h3>
-                <p>Side project</p>
+            <a
+              href="https://jammming-cyberbee.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={jammmingMockup}
+                alt="jammming-mockup"
+                className="Mockup"
+              />
+              <div className="lastproject-text-container">
+                <div className="lastproject-text">
+                  <h3>Jammming</h3>
+                  <p>Side project</p>
+                </div>
+                <img src={arrowmore} alt="" className="arrow-more" />
               </div>
-              <img src={arrowmore} alt="" className="arrow-more" />
-            </div>
+            </a>
           </div>
 
           <div className="lastprojectcard">
-            <img
-              src={coffeebeanMockup}
-              alt="coffeebean-mockup"
-              className="Mockup"
-            />
-            <div className="lastproject-text-container">
-              <div className="lastproject-text">
-                <h3>CoffeeBean</h3>
-                <p>Website design</p>
+            <a
+              href="https://cb-coffeebean.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={coffeebeanMockup}
+                alt="coffeebean-mockup"
+                className="Mockup"
+              />
+              <div className="lastproject-text-container">
+                <div className="lastproject-text">
+                  <h3>CoffeeBean</h3>
+                  <p>Website design</p>
+                </div>
+                <img src={arrowmore} alt="" className="arrow-more" />
               </div>
-              <img src={arrowmore} alt="" className="arrow-more" />
-            </div>
+            </a>
           </div>
         </div>
       </div>
